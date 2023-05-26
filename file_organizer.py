5221cdad6ca0ea9082f94e112fbfd6a62f8a60cf
Uemeyir <umeyir1998@googlemail.com>
2023-05-26 12:59:59 +0200

    simple file organizer

---
+++ file_organizer.py
@@ -0,0 +1,69 @@
+import os
+from pathlib import Path
+from sys import argv
+from shutil import move
+
+
+def get_dir(file):
+    f = str(file)
+    index_dot = f.rfind(".")
+    exten = f[index_dot:]
+    return dirs.get(exten, 'other')
+
+
+dirs = {
+    # Images
+    '.jpeg': 'Images',
+    '.png': 'Images',
+    '.gif': 'Images',
+    '.tiff': 'Images',
+
+    # Videos
+    '.mp4': 'Videos',
+    '.mpv': 'Videos',
+    '.avi': 'Videos',
+
+    # Documents
+    '.pdf': 'Documents',
+    '.rtf': 'Documents',
+    '.doc': 'Documents',
+    '.docx': 'Documents',
+    '.odt': 'Documents',
+    '.ppt': 'Documents',
+    '.xls': 'Documents',
+    '.zip': 'Documents',
+    '.txt': 'Documents',
+
+    # Program Files
+    '.exe': 'Program Files',
+    '.py': 'Program Files',
+    '.html': 'Program Files',
+    '.css': 'Program Files',
+    '.cpp': 'Program Files',
+    '.c': 'Program Files',
+    '.js':  'Program Files',
+}
+
+
+
+if len(argv) != 2:
+    print("Give one path to execute successfully")
+    exit(1)
+
+# The path to be organized
+PATH = Path(argv[1])
+
+# Iterate through the folder 
+for file in PATH.iterdir():
+    get_absolute_path = file.absolute()
+
+    if get_absolute_path.is_file():
+        # Get the last index from point
+        dir_name = get_dir(file)
+        destination = PATH / dir_name
+
+        # Make the destination folder if it doesn't exists
+        if not destination.exists():
+            destination.mkdir()
+        
+        move(str(get_absolute_path), str(destination))
